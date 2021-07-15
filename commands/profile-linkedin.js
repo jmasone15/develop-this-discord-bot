d0769a16ce07e5e48849aab49d1f6745e9c01d99
Jordan Masone <jordanmasone1@gmail.com>
2021-07-15 00:33:11 -0400

    Added a profile command to update your LinkedIn URL.

--- commands/profile-linkedin.js
+++ commands/profile-linkedin.js
@@ -1,12 +1,12 @@
 const Profile = require("../models/profileModel");
 
 module.exports = {
-    name: "profile-github",
-    description: "Profile command to update the github of your profile message.",
+    name: "profile-linkedin",
+    description: "Profile command to update the linkedin of your profile message.",
     guildOnly: false,
     args: true,
     usage: "<url>",
-    aliases: ["profiles-github", "profile-githubs"],
+    aliases: ["profiles-linkedin", "profile-linkedins"],
     cooldown: 3,
     async execute(message, args) {
         try {
@@ -19,18 +19,18 @@ module.exports = {
                 return message.reply("To use this command, you must first set up your profile with the command: \n\`$profile\`");
             };
 
-            // Validation to make sure the entered argument is a github link.
-            if (!args[0].startsWith("https://www.github.com/")) {
-                return message.reply("Make sure to enter a valid github link (https://github.com/username)");
+            // Validation to make sure the entered argument is a linkedin link.
+            if (!args[0].startsWith("https://www.linkedin.com/")) {
+                return message.reply("Make sure to enter a valid linkedin link (https://linkedin.com/username)");
             };
 
-            // Update the author's profile github.
-            await Profile.findByIdAndUpdate(existingProfile.id, { github: args[0] });
-            message.reply("Your github has been updated successfully!");
+            // Update the author's profile linkedin.
+            await Profile.findByIdAndUpdate(existingProfile.id, { linkedin: args[0] });
+            message.reply("Your linkedin has been updated successfully!");
 
         } catch (err) {
             console.error(err);
-            message.reply("Error using this command, please try again.")
+            message.reply("Error using this command, please try again.");
         }
     }
 }
